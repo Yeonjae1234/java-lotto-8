25bab1e5315e38ed32c8d7c35cc6164e06d22d91
yeonjae <yeoncom11@gmail.com>
2025-11-03 02:03:30 +0900

    feat: WinningChecker.checkTotalResult 구현

--- src/main/java/lotto/service/WinningChecker.java
+++ src/main/java/lotto/service/WinningChecker.java
@@ -4,31 +4,31 @@ import lotto.domain.Lotto;
 import lotto.domain.Rank;
 import lotto.domain.WinningNumbers;
 
-import java.util.LinkedHashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class WinningChecker {
-    Map<Rank,Integer> totalResult;
+    public Map<Rank,Integer> totalResult;
 
     public WinningChecker(){
-        totalResult = new LinkedHashMap<>();
-
-    }
-
-    public void checkTotalResult(List<Lotto> lottos, WinningNumbers winningNumbers) {
-
-    }
-
-    public void findRank(List<Lotto> lottos, WinningNumbers winningNumbers){
-        for (Lotto lotto : lottos) {
-            Rank result = winningNumbers.countMatchNumbers(lotto);
+        totalResult = new EnumMap<>(Rank.class);
+        for (Rank r : Rank.values()) {
+            totalResult.put(r, 0);
         }
     }
 
-    public Rank checkResult(Lotto lotto, WinningNumbers winningNumbers) {
-        return null;
+    public Map<Rank,Integer> checkTotalResult(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        for (Lotto lotto : lottos) {
+            findRank(lotto,winningNumbers);
+        }
+        return totalResult;
+    }
 
+    public void findRank(Lotto lotto, WinningNumbers winningNumbers){
+        Rank resultRank = winningNumbers.countMatchNumbers(lotto);
+        if(resultRank==null) return;
+        totalResult.put(resultRank, totalResult.get(resultRank) + 1);
     }
 
 
--- src/test/java/lotto/ServiceTest.java
+++ src/test/java/lotto/ServiceTest.java
@@ -2,11 +2,15 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.Rank;
+import lotto.domain.WinningNumbers;
 import lotto.service.LottoGenerator;
 import lotto.service.LottoListGenerator;
+import lotto.service.WinningChecker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -35,4 +39,48 @@ public class ServiceTest {
         });
 
     }
+
+    @Test
+    public void winningChecker_findRank_SECOND(){
+        assertSimpleTest(()->{
+            WinningChecker winningChecker = new WinningChecker();
+            Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            WinningNumbers winningNumbers = new WinningNumbers(winningLotto,7);
+            Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+            winningChecker.findRank(userLotto,winningNumbers);
+            assertThat(winningChecker.totalResult.get(Rank.SECOND)).isEqualTo(1);
+        });
+    }
+
+    @Test
+    public void winningChecker_findRank_NULL(){
+        assertSimpleTest(()->{
+            WinningChecker winningChecker = new WinningChecker();
+            Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            WinningNumbers winningNumbers = new WinningNumbers(winningLotto,7);
+            Lotto userLotto = new Lotto(List.of(8, 9, 10, 11, 12, 13));
+            winningChecker.findRank(userLotto,winningNumbers);
+            assertThat(winningChecker.totalResult.get(Rank.SECOND)).isEqualTo(0);
+        });
+    }
+
+    @Test
+    public void winningChecker_checkTotalResult(){
+        assertSimpleTest(()->{
+            WinningChecker winningChecker = new WinningChecker();
+            Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            WinningNumbers winningNumbers = new WinningNumbers(winningLotto,7);
+            ArrayList<Lotto> userLottos = new ArrayList<>();
+            userLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+            userLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+            userLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
+            userLottos.add(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
+            userLottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+            userLottos.add(new Lotto(List.of(7, 11, 30, 40, 42, 43)));
+            userLottos.add(new Lotto(List.of(2, 13, 22, 32, 38, 45)));
+            userLottos.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
+            winningChecker.checkTotalResult(userLottos,winningNumbers);
+        });
+    }
+
 }
